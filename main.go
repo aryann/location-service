a2bada1e1a9dc13df012ea3824fa506683bfc17c
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-11-14 15:34:26 -0800

    Adds code to compute the distance between two points.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/golang/geo/s2"
+)
+
+const (
+	radiusMeters = 6371010.0
+)
 
 func main() {
-	fmt.Println("hello")
+	a := s2.PointFromLatLng(s2.LatLngFromDegrees(47.618698, -122.320229))
+	b := s2.PointFromLatLng(s2.LatLngFromDegrees(47.615278, -122.320114))
+	fmt.Println(a.Distance(b).Radians() * radiusMeters)
 }
