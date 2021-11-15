da266bcc06fe04b33a902a28eb38d1ba85834678
Aryan Naraghi <aryan.naraghi@gmail.com>
2021-11-14 16:32:45 -0800

    Amends the benchmark comment.

--- locationservice/naive_test.go
+++ locationservice/naive_test.go
@@ -9,9 +9,9 @@ import (
 	"testing"
 )
 
-// BenchmarkFindClosest benchmarks the FindClosest() in NaiveLocationService
-// using 10,000 locations. NaiveLocationService performs a linear scan over the
-// locations.
+// BenchmarkFindClosest benchmarks the FindClosest() function in
+// NaiveLocationService using 10,000 locations. NaiveLocationService performs a
+// linear scan over the locations.
 //
 // On an Apple M1 computer, we expect results similar to:
 //
@@ -19,6 +19,14 @@ import (
 //
 // In other words, the average FindClosest() operation took around 0.219
 // milliseconds.
+//
+// Note that the implementation represents points as a 3D vector. As a result,
+// the distance calculation operations are significantly faster compared to a
+// representation that retains the original latitude and longitude. We expect an
+// implementation that does not use 3D vectors to be about three times slower:
+//
+//   BenchmarkFindClosest-8	1800	644824 ns/op	360 B/op	4 allocs/op
+//
 func BenchmarkFindClosest(b *testing.B) {
 	f, err := os.Open("test-data.csv")
 	if err != nil {
